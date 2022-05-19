ce19312855a1791c4a6d742eb587b29bc97e2ccd
fgfjfgnjg <104136695+fgfjfgnjg@users.noreply.github.com>
2022-05-19 11:27:48 +0530

    Update help.ts

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -8,6 +8,6 @@ export default class HelpCommand implements Command {
     module = 'General';
     
     execute = async(ctx: CommandContext) => {
-        await ctx.channel.send(`${process.env.DASHBOARD_URL}/commands`);
+        await ctx.channel.send(`**Under Development**, It will introduce soon!`);
     }
 }
